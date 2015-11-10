0a2d2be447cabade0f035500bd4be50928c2869d
Remco Tolsma <info@remcotolsma.nl>
2015-11-10 12:39:58 +0100

    composer install

--- src/GatewayIntegration.php
+++ src/GatewayIntegration.php
@@ -4,4 +4,16 @@ class Pronamic_WP_Pay_Gateways_Sisow_IDealBasic_GatewayIntegration {
 	public function __construct() {
 		$this->id = 'mollie-ideal-basic';
 	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
+	}
+
+	public function get_config_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
+	}
+
+	public function get_gateway_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
+	}
 }

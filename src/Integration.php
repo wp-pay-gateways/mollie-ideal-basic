8a39df4d5f961eb7ca6cd1a6eecf3fb103139c77
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 14:48:54 +0100

    Make sure to parent construct.

--- src/Integration.php
+++ src/Integration.php
@@ -2,6 +2,8 @@
 
 class Pronamic_WP_Pay_Gateways_Mollie_IDealBasic_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
 	public function __construct() {
+		parent::__construct();
+
 		$this->id            = 'mollie-ideal-basic';
 		$this->name          = 'Mollie - iDEAL Basic';
 		$this->dashboard_url = 'http://www.mollie.nl/';

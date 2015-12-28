34e2916abf1a110be03cf4905086f520e1ca8190
Remco Tolsma <info@remcotolsma.nl>
2015-12-28 14:25:33 +0100

    Added an test.

--- src/TestConfig.php
+++ src/TestConfig.php
@@ -8,7 +8,7 @@
  * @author Remco Tolsma
  * @version 1.0.0
  */
-class Pronamic_WP_Pay_Gateways_Mollie_IDealBasic_ConfigTest extends Pronamic_WP_Pay_Gateways_Mollie_IDealBasic_Config {
+class Pronamic_WP_Pay_Gateways_Mollie_IDealBasic_TestConfig extends Pronamic_WP_Pay_Gateways_Mollie_IDealBasic_Config {
 	public function get_payment_server_url() {
 		return 'https://secure.mollie.nl/xml/idealAcquirer/testmode/lite/';
 	}
